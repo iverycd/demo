d0f77998eb630074c0b0636f458d85803b088cad
Oracle.Kay <403668558@qq.com>
2020-09-23 15:45:53 +0800

    1111

---
+++ test2.py
@@ -0,0 +1,24 @@
+import binascii  
+a = b'hello world'
+b = binascii.b2a_hex(a)
+c = binascii.hexlify(a)
+d = binascii.a2b_hex('68656c6c6f20776f726c64')
+v = memoryview(b'abcefg')
+print(a)
+print(b)
+print(c)
+print(d)
+print(v[0],v[1],v[2],v[3])
+print(bytes(v))
+print('ddss')
+print('ssasx')
+'''
+
+print(b)  
+b'68656c6c6f20776f726c64'  
+b = binascii.hexlify(a)  
+print(b)  
+b'68656c6c6f20776f726c64'  
+print(binascii.unhexlify(b))  
+b'hello world'
+'''
